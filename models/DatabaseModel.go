6091ff6dc45c581cdc72de1c0c6f73135a6f51e5
abdanhafidz <abdan.hafidz@gmail.com>
2025-01-27 14:34:34 +0700

    refactor: JSON binding required and JSON formatting response

--- models/DatabaseModel.go
+++ models/DatabaseModel.go
@@ -7,118 +7,117 @@ import (
 )
 
 type ProblemSetAssign struct {
-	IDProblemSetAssign uint `gorm:"primaryKey"`
-	IDEvent            uint
-	IDProblemSet       uint
+	IDProblemSetAssign uint `gorm:"primaryKey" json:"id_problem_set_assign"`
+	IDEvent            uint `json:"id_event"`
+	IDProblemSet       uint `json:"id_problem_set"`
 }
 
 type Announcement struct {
-	IDAnnouncement uint `gorm:"primaryKey"`
-	Title          string
-	CreatedAt      time.Time
-	Message        string
-	Publisher      string
-	IDEvent        uint
+	IDAnnouncement uint      `gorm:"primaryKey" json:"id_announcement"`
+	Title          string    `json:"title"`
+	CreatedAt      time.Time `json:"created_at"`
+	Message        string    `json:"message"`
+	Publisher      string    `json:"publisher"`
+	IDEvent        uint      `json:"id_event"`
 }
 
 type Account struct {
-	IDAccount   uint `gorm:"primaryKey"`
-	Name        string
-	Username    string
-	Email       string
-	Password    string
-	PhoneNumber int
-	CreatedAt   time.Time
-	DeletedAt   time.Time
+	IDAccount   uint      `gorm:"primaryKey" json:"id_account"`
+	Name        string    `json:"name"`
+	Username    string    `json:"username"`
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	PhoneNumber int       `json:"phone_number"`
+	CreatedAt   time.Time `json:"created_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 }
 
 type Questions struct {
-	IDQuestion   uint   `gorm:"primaryKey"`
-	Type         string //MultChoices, ShortAns, Essay, IntPuzzle, IntType
-	Question     string
-	Options      []string `gorm:"type:text[]"`
-	AnsKey       []string `gorm:"type:text[]"`
-	CorrMark     float64
-	IncorrMark   float64
-	NullMark     float64
-	IDProblemSet uint
+	IDQuestion   uint     `gorm:"primaryKey" json:"id_question"`
+	Type         string   `json:"type"` //MultChoices, ShortAns, Essay, IntPuzzle, IntType
+	Question     string   `json:"question"`
+	Options      []string `gorm:"type:text[]" json:"options"`
+	AnsKey       []string `gorm:"type:text[]" json:"ans_key"`
+	CorrMark     float64  `json:"corr_mark"`
+	IncorrMark   float64  `json:"incorr_mark"`
+	NullMark     float64  `json:"null_mark"`
+	IDProblemSet uint     `json:"id_problem_set"`
 }
 
 // x = 1, x = 2, x = 3, x = 4
 // x++, x--, x+=1, x**, x/=1
 type ProblemSet struct {
-	IDProblemSet uint `gorm:"primaryKey"`
-	Title        string
-	Duration     time.Duration
-	Randomize    uint
-	MC_Count     uint
-	SA_Count     uint
-	Essay_Count  uint
+	IDProblemSet uint          `gorm:"primaryKey" json:"id_problem_set"`
+	Title        string        `json:"title"`
+	Duration     time.Duration `json:"duration"`
+	Randomize    uint          `json:"randomize"`
+	MC_Count     uint          `json:"mc_count"`
+	SA_Count     uint          `json:"sa_count"`
+	Essay_Count  uint          `json:"essay_count"`
 }
 
 type AccountDetails struct {
-	IDDetail    uint `gorm:"primaryKey"`
-	IDAccount   uint
-	Province    string
-	City        string
-	Institution string
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
+	IDDetail    uint      `gorm:"primaryKey" json:"id_detail"`
+	IDAccount   uint      `json:"id_account"`
+	Province    string    `json:"province"`
+	City        string    `json:"city"`
+	Institution string    `json:"institution"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 }
 
 type Events struct {
-	IDEvent    uint `gorm:"primaryKey"`
-	Title      string
-	StartEvent time.Time
-	EndEvent   time.Time
-	SID        string
-	Public     string
+	IDEvent    uint      `gorm:"primaryKey" json:"id_event"`
+	Title      string    `json:"title"`
+	StartEvent time.Time `json:"start_event"`
+	EndEvent   time.Time `json:"end_event"`
+	SID        string    `json:"sid"`
+	Public     string    `json:"public"`
 }
 
 type EventAssign struct {
-	IDAssign   uint `gorm:"primaryKey"`
-	IDAccount  uint
-	IDEvent    uint
-	AssignedAt time.Time
+	IDAssign   uint      `gorm:"primaryKey" json:"id_assign"`
+	IDAccount  uint      `json:"id_account"`
+	IDEvent    uint      `json:"id_event"`
+	AssignedAt time.Time `json:"assigned_at"`
 }
 
 type Result struct {
-	IDResult      uint `gorm:"primaryKey"`
-	IDAccount     uint
-	IDEvent       uint
-	IDProblemSet  uint
-	IDProgress    uint
-	FinishTime    time.Time
-	Correct       uint
-	Incorrect     uint
-	Empty         uint
-	OnCorrection  uint
-	ManualScoring float64
-	MCScore       float64
-	ManualScore   float64
-	FinalScore    float64
+	IDResult      uint      `gorm:"primaryKey" json:"id_result"`
+	IDAccount     uint      `json:"id_account"`
+	IDEvent       uint      `json:"id_event"`
+	IDProblemSet  uint      `json:"id_problem_set"`
+	IDProgress    uint      `json:"id_progress"`
+	FinishTime    time.Time `json:"finish_time"`
+	Correct       uint      `json:"correct"`
+	Incorrect     uint      `json:"incorrect"`
+	Empty         uint      `json:"empty"`
+	OnCorrection  uint      `json:"on_correction"`
+	ManualScoring float64   `json:"manual_scoring"`
+	MCScore       float64   `json:"mc_score"`
+	ManualScore   float64   `json:"manual_score"`
+	FinalScore    float64   `json:"final_score"`
 }
 
 type ExamProgress struct {
-	IDProgress     uint `gorm:"primaryKey"`
-	IDAccount      uint
-	IDEvent        uint
-	IDProblemSet   uint
-	CreatedAt      time.Time
-	DueAt          time.Time
-	QuestionsOrder []string `gorm:"type:text[]"`
-	Answers        any      `gorm:"type:jsonb"`
+	IDProgress     uint      `gorm:"primaryKey" json:"id_progress"`
+	IDAccount      uint      `json:"id_account"`
+	IDEvent        uint      `json:"id_event"`
+	IDProblemSet   uint      `json:"id_problem_set"`
+	CreatedAt      time.Time `json:"created_at"`
+	DueAt          time.Time `json:"due_at"`
+	QuestionsOrder []string  `gorm:"type:text[]" json:"questions_order"`
+	Answers        any       `gorm:"type:jsonb" json:"answers"`
 }
-type JSONB []interface{}
 type ExamProgress_Result struct {
-	IDProgress     uint `gorm:"primaryKey"`
-	IDAccount      uint
-	IDEvent        uint
-	IDProblemSet   uint
-	CreatedAt      time.Time
-	DueAt          time.Time
-	QuestionsOrder []string       `gorm:"type:text[]"`
-	Answers        postgres.Jsonb `gorm:"type:jsonb"`
+	IDProgress     uint           `gorm:"primaryKey" json:"id_progress"`
+	IDAccount      uint           `json:"id_account"`
+	IDEvent        uint           `json:"id_event"`
+	IDProblemSet   uint           `json:"id_problem_set"`
+	CreatedAt      time.Time      `json:"created_at"`
+	DueAt          time.Time      `json:"due_at"`
+	QuestionsOrder []string       `gorm:"type:text[]" json:"questions_order"`
+	Answers        postgres.Jsonb `gorm:"type:jsonb" json:"answers"`
 }
 
 // Gorm table name settings
--- models/RequestModel.go
+++ models/RequestModel.go
@@ -1,47 +1,47 @@
 package models
 
 type LoginRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type RegisterRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Phone    int    `json:"phone"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Phone    int    `json:"phone" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type EventListRequest struct {
-	PerPage    int    `json:"per_page"`
-	PageNumber int    `json:"page_number"`
-	Filter     string `json:"filter"`
+	PerPage    int    `json:"per_page" binding:"required"`
+	PageNumber int    `json:"page_number" binding:"required"`
+	Filter     string `json:"filter"` // Optional, tidak diberi `required` jika tidak wajib.
 }
 
 type EventDetailRequest struct {
-	IdEvent int `json:"id_event"`
+	IdEvent int `json:"id_event" binding:"required"`
 }
 
 type ProblemSetRequest struct {
-	IdEvent int `json:"id_event"`
+	IdEvent int `json:"id_event" binding:"required"`
 }
 
 type ExamRequest struct {
-	IdEvent      int `json:"id_event"`
-	IdProblemSet int `json:"id_problem_set"`
+	IdEvent      int `json:"id_event" binding:"required"`
+	IdProblemSet int `json:"id_problem_set" binding:"required"`
 }
 
 type RegisterEventRequest struct {
-	IdEvent   int    `json:"id_event"`
-	EventCode string `json:"event_code"`
+	IdEvent   int    `json:"id_event" binding:"required"`
+	EventCode string `json:"event_code" binding:"required"`
 }
 
 type QuestionRequest struct {
-	IdProblemSet int `json:"id_problem_set"`
+	IdProblemSet int `json:"id_problem_set" binding:"required"`
 }
 
 type SubmitRequest struct {
-	IdEvent      int `json:"id_event"`
-	IdProblemSet int `json:"id_problem_set"`
+	IdEvent      int `json:"id_event" binding:"required"`
+	IdProblemSet int `json:"id_problem_set" binding:"required"`
 }
