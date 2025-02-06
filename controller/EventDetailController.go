bbe3329c6d2a952631870ca1de1dc841654c251d
ABDAN HAFIDZ <71253590+abdanhafidz@users.noreply.github.com>
2025-02-06 21:04:51 +0700

    Update EventDetailController.go

--- controller/EventDetailController.go
+++ controller/EventDetailController.go
@@ -12,7 +12,7 @@ import (
 func EventDetailController(c *gin.Context) {
 
 	var req models.EventDetailRequest
-	c.ShouldBindJSON(&req)
+	c.ShouldBind(&req)
 
 	var account models.AccountData
 	cParam, _ := c.Get("accountData")
